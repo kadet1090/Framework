17e0581ad3d8b2e20cd35dee959445fd53c2b6e1
Kadet <kadet1090@gmail.com>
2014-06-02 21:15:01 +0200

    Dispatch for router.

--- Framework/Http/HttpRequest.php
+++ Framework/Http/HttpRequest.php
@@ -1,59 +1,65 @@
 <?php
 
-namespace Framework\Router;
+namespace Framework\Http;
 
 use Framework\Data\Request;
 
 class HttpRequest extends Request
 {
-	protected $_parameters;
+    protected $_parameters;
+    protected $_input;
 
-	public function __construct(array $parameters)
-	{
-		$this->_parameters = $parameters;
-	}
+    public function __construct(array $parameters, array $input)
+    {
+        $this->_parameters = $parameters;
+        $this->_input      = $input;
+    }
 
-	public function __get($name)
-	{
-		if (!isset($this->_parameters[$name])) {
-			return null;
-		}
-		return $this->_parameters[$name];
-	}
+    public function __get($name)
+    {
+        if (!isset($this->_input[$name])) {
+            return null;
+        }
 
-	/**
-	 * (PHP 5 &gt;= 5.0.0)<br/>
-	 * Whether a offset exists
-	 *
-	 * @link http://php.net/manual/en/arrayaccess.offsetexists.php
-	 * @param mixed $offset <p>
-	 * An offset to check for.
-	 * </p>
-	 * @return boolean true on success or false on failure.
-	 * </p>
-	 * <p>
-	 * The return value will be casted to boolean if non-boolean was returned.
-	 */
-	public function offsetExists($offset)
-	{
-		return is_int($offset) && isset($this->_parameters[$offset]);
-	}
+        return $this->_input[$name];
+    }
 
-	/**
-	 * (PHP 5 &gt;= 5.0.0)<br/>
-	 * Offset to retrieve
-	 *
-	 * @link http://php.net/manual/en/arrayaccess.offsetget.php
-	 * @param mixed $offset <p>
-	 * The offset to retrieve.
-	 * </p>
-	 * @return mixed Can return all value types.
-	 */
-	public function offsetGet($offset)
-	{
-		if (!isset($this->_parameters[$offset])) {
-			return null;
-		}
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Whether a offset exists
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     *
+     * @param mixed $offset <p>
+     *                      An offset to check for.
+     *                      </p>
+     *
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     */
+    public function offsetExists($offset)
+    {
+        return is_int($offset) && isset($this->_parameters[$offset]);
+    }
 
-	}
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Offset to retrieve
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to retrieve.
+     *                      </p>
+     *
+     * @return mixed Can return all value types.
+     */
+    public function offsetGet($offset)
+    {
+        if (!isset($this->_parameters[$offset])) {
+            return null;
+        }
+    }
 }
--- Framework/Http/HttpRouter.php
+++ Framework/Http/HttpRouter.php
@@ -1,152 +1,174 @@
 <?php
-
-namespace Framework\Router;
+namespace Framework\Http;
 
 use Framework\Exceptions\RoutingException;
 use Framework\Interfaces\RouterInterface;
 
 class HttpRouter implements RouterInterface
 {
-	/**
-	 * Stores routes and its regex.
-	 *
-	 * @var array[string]
-	 */
-	private $_routes = [];
+    /**
+     * Stores routes and its regex.
+     *
+     * @var array[string]
+     */
+    private $_routes = [];
 
-	/**
-	 * Url parameters separator
-	 * Can be regex value, remember to use non-capturing group!
-	 *
-	 * @var string
-	 */
-	public $separator = '/';
+    /**
+     * Url parameters separator
+     * Can be regex value, remember to use non-capturing group!
+     *
+     * @var string
+     */
+    public $separator = '/';
 
-	/**
-	 * File extension.
-	 * Can be regex value, remember to use non-capturing group!
-	 *
-	 * @var string
-	 */
-	public $extension = '(:?\.php)?';
+    /**
+     * File extension.
+     * Can be regex value, remember to use non-capturing group!
+     *
+     * @var string
+     */
+    public $extension = '(:?\.php)?';
 
-	/**
-	 * Turn request string into Request.
-	 *
-	 * @param string $string Request string to dispatch.
-	 * @return HttpRequest Request data.
-	 */
-	public function dispatch($string)
-	{
+    /**
+     * Turn request string into Request.
+     *
+     * @param string $string Request string to dispatch.
+     *
+     * @return HttpRequest Request data.
+     */
+    public function dispatch($string)
+    {
+        $input      = $_POST;
+        $parameters = [];
 
-	}
+        foreach ($this->_routes as $route) {
+            preg_match($route['regex'], $string, $matches);
+            $parameters = array_merge($parameters, $matches);
+        }
 
-	/**
-	 * Add new route
-	 *
-	 * @param string $name Route name.
-	 * @param array $route Route data. <p>
-	 * array(
-	 *      'parameters' => array(
-	 *          'first'  => 'string',
-	 *          'second' => 'int',
-	 *          'third'  => '0x[0-9A-Fa-f]+',
-	 *      ),
-	 *      'optional' => array(
-	 *          'fourth' => 'string',
-	 *          array(
-	 *              'fifth' => 'string',
-	 *              'sixth' => 'int'
-	 *          ),
-	 *      ),
-	 *      'parent' => 'parent route',
-	 * )
-	 * </p>
-	 * @throws RoutingException
-	 * @return void
-	 */
-	public function addRoute($name, array $route)
-	{
-		if (!isset($route['optional'])) {
-			$route['optional'] = [];
-		}
-		if (!isset($route['parameters'])) {
-			$route['parameters'] = [];
-		}
+        return new HttpRequest($parameters, $input);
+    }
 
-		if (isset($route['parent'])) {
-			if (!isset($this->_routes[$route['parent']])) {
-				throw new RoutingException('Route depends on nonexistent route.');
-			}
+    /**
+     * Add new route
+     *
+     * @param string $name  Route name.
+     * @param array  $route Route data. <p>
+     *                      [
+     *                      'parameters' => [
+     *                      'first'  => 'string',
+     *                      'second' => 'int',
+     *                      'third'  => '0x[0-9A-Fa-f]+',
+     *                      ],
+     *                      'optional' => [
+     *                      'fourth' => 'string',
+     *                      [
+     *                      'fifth' => 'string',
+     *                      'sixth' => 'int'
+     *                      ],
+     *                      ],
+     *                      'parent' => 'parent route',
+     *                      ]
+     *                      </p>
+     *
+     * @throws RoutingException
+     * @return void
+     */
+    public function addRoute($name, array $route)
+    {
+        if (!isset($route['optional'])) {
+            $route['optional'] = [];
+        }
+        if (!isset($route['parameters'])) {
+            $route['parameters'] = [];
+        }
 
-			$route['parameters'] = array_merge($this->_routes[$route['parent']]['data']['parameters'], $route['parameters']);
-			$route['optional'] = array_merge($this->_routes[$route['parent']]['data']['optional'], $route['optional']);
-		}
+        if (isset($route['parent'])) {
+            if (!isset($this->_routes[$route['parent']])) {
+                throw new RoutingException('Route depends on nonexistent route.');
+            }
 
-		$this->_routes[$name] = [
-			'data' => $route,
-			'regex' => $this->_compileRegex($route),
-		];
-	}
+            $route['parameters'] = array_merge(
+                $this->_routes[$route['parent']]['data']['parameters'],
+                $route['parameters']
+            );
 
-	/**
-	 * Check if request matches specified route.
-	 *
-	 * @param string $route Route to match.
-	 * @param string $request Request string.
-	 * @return bool If true $request matches given route, else false.
-	 */
-	public function match($route, $request)
-	{
-		if (!isset($this->_routes[$route])) {
-			return false;
-		}
-		return (bool)preg_match($this->_routes[$route]['regex'], $request);
-	}
+            $route['optional'] = array_merge(
+                $this->_routes[$route['parent']]['data']['optional'],
+                $route['optional']
+            );
+        }
 
-	/**
-	 * Turns route into regex.
-	 *
-	 * @param array $route Route data.
-	 * @return string Compiled regex.
-	 */
-	private function _compileRegex(array $route)
-	{
-		$regex = '';
-		if (isset($route['parameters'])) {
-			foreach ($route['parameters'] as $name => $pattern) {
-				$regex .= $this->_getRegexParameter($name, $pattern);
-			}
-		}
-		$regex = substr($regex, 1);
+        $this->_routes[$name] = [
+            'data'  => $route,
+            'regex' => $this->_compileRegex($route),
+        ];
+    }
 
-		if (isset($route['optional'])) {
-			foreach ($route['optional'] as $name => $pattern) {
-				$regex .= '(?:$|';
-				if (is_array($pattern)) {
-					foreach ($pattern as $subname => $subpattern) {
-						$regex .= $this->_getRegexParameter($subname, $subpattern);
-					}
-				} else {
-					$regex .= $this->_getRegexParameter($name, $pattern);
-				}
-			}
-			$regex .= str_repeat(')', count($route['optional']));
-		}
-		$regex .= $this->extension;
-		return '#^' . $regex . '#si';
-	}
+    /**
+     * Check if request matches specified route.
+     *
+     * @param string $route   Route to match.
+     * @param string $request Request string.
+     *
+     * @return bool If true $request matches given route, else false.
+     */
+    public function match($route, $request)
+    {
+        if (!isset($this->_routes[$route])) {
+            return false;
+        }
 
-	/**
-	 * Gets regex for specified parameter.
-	 *
-	 * @param string $name Parameters' name
-	 * @param string $pattern Parameters' pattern/type (int is [0-9]+, string [a-zA-Z0-9\-\_\+]+ etc.)
-	 * @return string
-	 */
-	private function _getRegexParameter($name, $pattern)
-	{
-		$pattern = str_replace(['int', 'string'], ['[0-9]+', '[a-zA-Z0-9\-\_\+]+'], $pattern);
-		return "{$this->separator}(?P<$name>$pattern)";
-	}
+        return (bool)preg_match($this->_routes[$route]['regex'], $request);
+    }
+
+    /**
+     * Turns route into regex.
+     *
+     * @param array $route Route data.
+     *
+     * @return string Compiled regex.
+     */
+    private function _compileRegex(array $route)
+    {
+        $regex = '';
+        if (isset($route['parameters'])) {
+            foreach ($route['parameters'] as $name => $pattern) {
+                $regex .= $this->_getRegexParameter($name, $pattern);
+            }
+        }
+        $regex = substr($regex, 1);
+
+        if (isset($route['optional'])) {
+            foreach ($route['optional'] as $name => $pattern) {
+                $regex .= '(?:$|';
+                if (is_array($pattern)) {
+                    foreach ($pattern as $subname => $subpattern) {
+                        $regex .= $this->_getRegexParameter($subname, $subpattern);
+                    }
+                } else {
+                    $regex .= $this->_getRegexParameter($name, $pattern);
+                }
+            }
+            $regex .= str_repeat(')', count($route['optional']));
+        }
+        $regex .= $this->extension;
+
+        return '#^' . $regex . '#si';
+    }
+
+    /**
+     * Gets regex for specified parameter.
+     *
+     * @param string $name    Parameters' name
+     * @param string $pattern Parameters' pattern/type (int is [0-9]+, string [a-zA-Z0-9\-\_\+]+ etc.)
+     *
+     * @return string
+     */
+    private function _getRegexParameter($name, $pattern)
+    {
+        $pattern = str_replace(['int', 'string'], ['[0-9]+', '[a-zA-Z0-9\-\_\+]+'], $pattern);
+
+        return "{$this->separator}(?P<$name>$pattern)";
+    }
 }
